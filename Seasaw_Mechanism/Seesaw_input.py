79510878722bf8b103ced4c7f17e29c65a68611e
Cash Coffman <116964726+Moneydollar@users.noreply.github.com>
2023-01-08 23:33:26 -0500

    I am trying to implement kinematic formulas in order develop the basic math for the Seesaw balance Seasaw_Mechanism.

--- Seasaw_Mechanism/Seesaw_input.py
+++ Seasaw_Mechanism/Seesaw_input.py
@@ -1,1 +1,6 @@
+import robotpy 
+import wpilib
 #In Progress
+# formula for the accleration of gravity is y = y0 + vy0t - ½ g*t^2
+def accleration_of_gravity(inital_position,inital_vert_speed, gravity, time):
+    
