18175e4f7a8eab5eb0acd262ac4267053b10121f
Cash Coffman <116964726+Moneydollar@users.noreply.github.com>
2023-01-17 18:04:58 -0500

    Added Pizzabot code for teleop. Working on state-based Autounomus_Framework. Teleop is now operational on Pizzabot.

---
+++ mwe/__pycache__/robot.py
@@ -0,0 +1,18 @@
+import wpilib
+
+class MyRobot(wpilib.TimedRobot):
+
+    def robotInit(self) -> None:
+        return super().robotInit()
+    
+    def autonomousInit(self) -> None:
+        return super().autonomousInit()
+    
+    def autonomousPeriodic(self) -> None:
+        return super().autonomousPeriodic()
+    
+    def teleopInit(self) -> None:
+        return super().teleopInit()
+    
+    def teleopPeriodic(self) -> None:
+        return super().teleopPeriodic()
